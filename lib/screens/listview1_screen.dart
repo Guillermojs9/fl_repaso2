b494fcf10f8cf5f6e707e3186e11330583dab42c
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-09 11:22:34 +0100

    Primera iteración LIstView1

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final equipos = const [
+    'Granada CF',
+    'Malaga CF',
+    'UD Almeria',
+    'Albacete BP'
+  ];
+  const Listview1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("ListView Tipo 1"),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: ListView(
+          children: [
+            ...equipos.map(
+              (e) => ListTile(
+                title: Text(e),
+                //leading: const Icon(Icons.access_time),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              ),
+            )
+          ],
+        ));
+  }
+}

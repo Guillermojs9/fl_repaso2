d21ebbfa32284606ffae21cb2ea324771071ff7d
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-09 11:44:36 +0100

    screens.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:fl_repaso/screens/listview1_screen.dart';
-import 'package:fl_repaso/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
+import 'screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,10 +8,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Listview2Screen()
+      //home: const Listview2Screen(),
+      initialRoute: 'home',
+      routes: {
+        'listview1': (BuildContext context) => const Listview1Screen(),
+        'listview2': (BuildContext context) => const Listview2Screen(),
+        'alert': (BuildContext context) => const AlertScreen(),
+        'card': (BuildContext context) => const CardScreen(),
+        'home': (BuildContext context) => const HomeScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:fl_repaso/screens/alert_screen.dart';
+export 'package:fl_repaso/screens/card_screen.dart';
+export 'package:fl_repaso/screens/home_screen.dart';
+export 'package:fl_repaso/screens/listview1_screen.dart';
+export 'package:fl_repaso/screens/listview2_screen.dart';
